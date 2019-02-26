db29d4df5b90ea5552a3be5c9192069575f764e3
Jon Salenger <greifinn17@gmail.com>
2019-02-26 16:59:50 -0500

    Heroku try:X+5

--- newspaper_project/settings.py
+++ newspaper_project/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '4dc-1$4=qz!i6zw%pbf3gmssr_^#=8@qhjrbwm+!meaqv+p9(d'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
